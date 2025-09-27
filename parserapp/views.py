0da4c2a413306b3ef7bda45b4af41538257a7230
Narelsiak <michalekxddlol@gmail.com>
2025-09-27 13:06:07 +0200

    views & upload front

--- parserapp/views.py
+++ parserapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.core.files.uploadedfile import UploadedFile
 
-# Create your views here.
+def parse_file(request: HttpRequest) -> HttpResponse:
+    return render(request, 'upload.html')
